276d91901f288502076713efa6568a2d62ae74bb
YucongHe <heyucong961028@berkeley.edu>
2016-11-11 17:10:29 -0800

    First Test: Softmax

---
+++ tensorflow_test1.py
@@ -0,0 +1,37 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# Describe interacting ops by manipulating symbolic variables.
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]));
+
+# implement our model!
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# place holder for correct answers.
+_y = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(_y * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# Initialize the variables we create.
+# It defines the op but didn't run!
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, _y:batch_ys})
+
+# Evaluating the model!
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(_y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x:mnist.test.images, _y: mnist.test.labels}))
